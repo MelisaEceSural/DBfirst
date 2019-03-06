47e2091e59eb6d8f9daa2af5f19c45c7795647cc
MELİSA ECE SURAL <47081681+MelisaEceSural@users.noreply.github.com>
2019-03-06 13:49:47 +0300

    Add files via upload

---
+++ DbFirst_20190227/DbFirst_20190227/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace DbFirst_20190227
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numIndirim = new System.Windows.Forms.NumericUpDown();
+            this.numAdet = new System.Windows.Forms.NumericUpDown();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.lstSepet = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSiparisVer = new System.Windows.Forms.Button();
+            this.cmbNakliye = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbMusteri = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbPersonel = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numIndirim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAdet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(986, 287);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numIndirim);
+            this.groupBox1.Controls.Add(this.numAdet);
+            this.groupBox1.Controls.Add(this.txtAra);
+            this.groupBox1.Controls.Add(this.lstSepet);
+            this.groupBox1.Controls.Add(this.btnCikar);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.btnSiparisVer);
+            this.groupBox1.Controls.Add(this.cmbNakliye);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cmbMusteri);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbPersonel);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 293);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(986, 232);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sipariş İşlemleri";
+            // 
+            // numIndirim
+            // 
+            this.numIndirim.Location = new System.Drawing.Point(567, 40);
+            this.numIndirim.Name = "numIndirim";
+            this.numIndirim.Size = new System.Drawing.Size(100, 30);
+            this.numIndirim.TabIndex = 5;
+            // 
+            // numAdet
+            // 
+            this.numAdet.Location = new System.Drawing.Point(437, 40);
+            this.numAdet.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numAdet.Name = "numAdet";
+            this.numAdet.Size = new System.Drawing.Size(100, 30);
+            this.numAdet.TabIndex = 5;
+            this.numAdet.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(300, 40);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 30);
+            this.txtAra.TabIndex = 4;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // lstSepet
+            // 
+            this.lstSepet.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lstSepet.FullRowSelect = true;
+            this.lstSepet.GridLines = true;
+            this.lstSepet.Location = new System.Drawing.Point(290, 76);
+            this.lstSepet.MultiSelect = false;
+            this.lstSepet.Name = "lstSepet";
+            this.lstSepet.Size = new System.Drawing.Size(689, 144);
+            this.lstSepet.TabIndex = 3;
+            this.lstSepet.UseCompatibleStateImageBehavior = false;
+            this.lstSepet.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ürün Adı";
+            this.columnHeader1.Width = 297;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Fiyat";
+            this.columnHeader2.Width = 118;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Adet";
+            this.columnHeader3.Width = 71;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "İndirim %";
+            this.columnHeader4.Width = 104;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Tutar";
+            this.columnHeader5.Width = 93;
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikar.ForeColor = System.Drawing.Color.Black;
+            this.btnCikar.Location = new System.Drawing.Point(826, 29);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(148, 37);
+            this.btnCikar.TabIndex = 2;
+            this.btnCikar.Text = "Sepetten Çıkar";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.Color.Black;
+            this.btnEkle.Location = new System.Drawing.Point(684, 29);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(136, 37);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Sepete Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSiparisVer
+            // 
+            this.btnSiparisVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiparisVer.ForeColor = System.Drawing.Color.Red;
+            this.btnSiparisVer.Location = new System.Drawing.Point(98, 170);
+            this.btnSiparisVer.Name = "btnSiparisVer";
+            this.btnSiparisVer.Size = new System.Drawing.Size(173, 50);
+            this.btnSiparisVer.TabIndex = 2;
+            this.btnSiparisVer.Text = "Sipariş Ver";
+            this.btnSiparisVer.UseVisualStyleBackColor = true;
+            this.btnSiparisVer.Click += new System.EventHandler(this.btnSiparisVer_Click);
+            // 
+            // cmbNakliye
+            // 
+            this.cmbNakliye.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNakliye.FormattingEnabled = true;
+            this.cmbNakliye.Location = new System.Drawing.Point(98, 123);
+            this.cmbNakliye.Name = "cmbNakliye";
+            this.cmbNakliye.Size = new System.Drawing.Size(173, 30);
+            this.cmbNakliye.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nakliye";
+            // 
+            // cmbMusteri
+            // 
+            this.cmbMusteri.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMusteri.FormattingEnabled = true;
+            this.cmbMusteri.Location = new System.Drawing.Point(98, 76);
+            this.cmbMusteri.Name = "cmbMusteri";
+            this.cmbMusteri.Size = new System.Drawing.Size(173, 30);
+            this.cmbMusteri.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(563, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 22);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "İndirim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Müşteri";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(434, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 22);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Adet";
+            // 
+            // cmbPersonel
+            // 
+            this.cmbPersonel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPersonel.FormattingEnabled = true;
+            this.cmbPersonel.Location = new System.Drawing.Point(98, 30);
+            this.cmbPersonel.Name = "cmbPersonel";
+            this.cmbPersonel.Size = new System.Drawing.Size(173, 30);
+            this.cmbPersonel.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ürün Ara";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 525);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numIndirim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAdet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numIndirim;
+        private System.Windows.Forms.NumericUpDown numAdet;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.ListView lstSepet;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSiparisVer;
+        private System.Windows.Forms.ComboBox cmbNakliye;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbMusteri;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbPersonel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DbFirst_20190227/DbFirst_20190227/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbFirst_20190227
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Northwind2Entities data = new Northwind2Entities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.DataSource = data.Urunler.Where(x => x.Sonlandi == false).ToList();
+
+            getDb();
+        }
+        void getProducts()
+        {
+            dataGridView1.DataSource = (from urun in data.Urunler.Where(x => x.Sonlandi == false && x.HedefStokDuzeyi > 0)
+                                        join kat in data.Kategoriler
+                                        on urun.KategoriID equals kat.KategoriID
+                                        join ted in data.Tedarikciler
+                                        on urun.TedarikciID equals ted.TedarikciID
+                                        select new
+                                        {
+                                            Id = urun.UrunID,
+                                            Adi = urun.UrunAdi,
+                                            Fiyat = urun.BirimFiyati,
+                                            Stok = urun.HedefStokDuzeyi,
+                                            kat.KategoriAdi,
+                                            urun.KategoriID,
+                                            ted.SirketAdi,
+                                            urun.TedarikciID
+                                        }).ToList();
+
+
+            numIndirim.Value = 0;
+
+        }
+
+        void getProducts(string aranan)
+        {
+            dataGridView1.DataSource = (from urun in data.Urunler.Where(x => x.Sonlandi == false && x.HedefStokDuzeyi > 0)
+                                        join kat in data.Kategoriler
+                                        on urun.KategoriID equals kat.KategoriID
+                                        join ted in data.Tedarikciler
+                                        on urun.TedarikciID equals ted.TedarikciID
+                                        select new
+                                        {
+                                            Id = urun.UrunID,
+                                            Adi = urun.UrunAdi,
+                                            Fiyat = urun.BirimFiyati,
+                                            Stok = urun.HedefStokDuzeyi,
+                                            kat.KategoriAdi,
+                                            urun.KategoriID,
+                                            ted.SirketAdi,
+                                            urun.TedarikciID
+                                        }).Where(x=>x.Adi.Contains(aranan)).ToList();
+
+
+            numIndirim.Value = 0;
+
+        }
+        void getDb()
+        {
+            getProducts();
+
+
+
+            cmbPersonel.DataSource = data.Personeller.Select(p => new
+            {
+                Personel = p.Adi + " " + p.SoyAdi,
+                Id = p.PersonelID
+            }).ToList();
+            cmbPersonel.DisplayMember = "Personel";
+            cmbPersonel.ValueMember = "Id";
+
+            cmbMusteri.DataSource = data.Musteriler.Select(m => new
+            {
+                m.SirketAdi,
+                m.MusteriID
+            }).ToList();
+            cmbMusteri.DisplayMember = "SirketAdi";
+            cmbMusteri.ValueMember = "MusteriID";
+
+            cmbNakliye.DataSource = data.Nakliyeciler.ToList();
+            cmbNakliye.DisplayMember = "SirketAdi";
+            cmbNakliye.ValueMember = "NakliyeciID";
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Lütfen bir ürün seçin", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            DataGridViewRow satir = dataGridView1.CurrentRow;
+
+            numAdet.Maximum = (short)satir.Cells[3].Value;
+
+            ListViewItem li = new ListViewItem();
+            li.Tag = satir.Cells[0].Value;
+            li.Text = satir.Cells[1].Value.ToString();
+            li.SubItems.Add(satir.Cells[2].Value.ToString());
+            // Adet ve indirimi elle giriyorduk
+            li.SubItems.Add(numAdet.Value.ToString());
+            li.SubItems.Add(numIndirim.Value.ToString());
+            decimal tutar = Convert.ToDecimal(satir.Cells[2].Value.ToString()) * numAdet.Value * (1 - (numIndirim.Value / 100));
+            li.SubItems.Add(tutar.ToString());
+
+            foreach (ListViewItem item in lstSepet.Items)
+            {
+                if ((int)li.Tag == (int)item.Tag)
+                {
+                    MessageBox.Show($"{li.Text} adlı ürün sepette mevcut.");
+                    return;
+                }
+            }
+
+
+            lstSepet.Items.Add(li);
+            numAdet.Value = 1;
+            getProducts();
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            if (lstSepet.SelectedItems.Count == 0) return;
+            lstSepet.Items.Remove(lstSepet.SelectedItems[0]);
+        }
+
+        private void btnSiparisVer_Click(object sender, EventArgs e)
+        {
+            if (lstSepet.Items.Count < 1)
+            {
+                MessageBox.Show("Sepette hiç ürün yok.");
+                return;
+            }
+
+            if (cmbMusteri.SelectedItem == null || cmbNakliye.SelectedItem == null || cmbPersonel.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen gerekli alanları doldurun.");
+                return;
+            }
+
+            // ARTIK SATIŞ YAPABİLİRİM
+            Satislar yeniSatis = new Satislar();
+            yeniSatis.SatisTarihi = DateTime.Today;
+            yeniSatis.PersonelID = (int)cmbPersonel.SelectedValue;
+            yeniSatis.MusteriID = cmbMusteri.SelectedValue.ToString();
+            yeniSatis.ShipVia = (int)cmbNakliye.SelectedValue;
+            yeniSatis.SevkTarihi = DateTime.Today.AddDays(7);
+            data.Satislar.Add(yeniSatis);
+            //Master bilgileri girdik, sırada detay bilgiler var
+            foreach (ListViewItem urun in lstSepet.Items)
+            {
+                Satis_Detaylari sd = new Satis_Detaylari();
+                sd.SatisID = yeniSatis.SatisID;
+                sd.UrunID = (int)urun.Tag;
+                sd.BirimFiyati = decimal.Parse(urun.SubItems[1].Text);
+                sd.Miktar = short.Parse(urun.SubItems[2].Text);
+                sd.İndirim = float.Parse(urun.SubItems[3].Text) / 100;
+
+                data.Satis_Detaylari.Add(sd);
+            }
+            if (data.SaveChanges() > 0)
+            {
+                MessageBox.Show($"{yeniSatis.SatisID} Id'li satış gerçekleşti.");
+                lstSepet.Items.Clear();
+            }
+            else
+                MessageBox.Show("HATA");
+
+            getProducts();
+
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+            getProducts(txtAra.Text);
+        }
+    }
+}
